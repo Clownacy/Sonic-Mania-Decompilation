645025cc1aa7504bf3b1a842fc837393ba50e287
Clownacy <Clownacy@users.noreply.github.com>
2022-09-26 14:46:52 +0100

    Disable the Special Stage cheat button.
    It can still be used if Debug Mode is enabled. I just don't like the
    idea of someone accidentally pressing that button and spoiling the
    challenge for themself.

--- SonicMania/Objects/UFO/UFO_HUD.c
+++ SonicMania/Objects/UFO/UFO_HUD.c
@@ -14,7 +14,8 @@ void UFO_HUD_Update(void)
     RSDK_THIS(UFO_HUD);
 
 #if MANIA_USE_PLUS && GAME_VERSION != VER_107
-    if (ControllerInfo->keyY.press) {
+    // Not original code, prevents cheating unless using Debug Mode
+    if (globals->medalMods & MEDAL_DEBUGMODE && ControllerInfo->keyY.press) {
         // Not original code, should help fix some crashes/glitches that occur due to how this works through
         Entity *selfStore = SceneInfo->entity;
         SceneInfo->entity = RSDK_GET_ENTITY_GEN(SLOT_PLAYER1);
